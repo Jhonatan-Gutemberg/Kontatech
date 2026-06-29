8b9c7660e841564b5686a608cce782e86b44e260
Jhonatan-Gutemberg <mecjgr@hotmail.com>
2026-06-28 21:58:33 -0300

    feat: update API base URL and WebSocket notification URL handling

--- code/mobile/kontatech/lib/config/api_config.dart
+++ code/mobile/kontatech/lib/config/api_config.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/foundation.dart';
 
 String get apiBaseUrl {
+  return 'https://pmg-es-2025-2-ti5-6904100-kontatech.onrender.com';
+
+  /*
   if (kIsWeb) {
     return 'http://localhost:8000';
   }
-
   switch (defaultTargetPlatform) {
     case TargetPlatform.android:
       return 'http://10.0.2.2:8000';
@@ -13,9 +15,14 @@ String get apiBaseUrl {
     default:
       return 'http://192.168.1.11:8000';
   }
+  */
 }
 
 String apiUrl(String path) => '$apiBaseUrl$path';
 
-String wsNotificationsUrl(String jwt) =>
-    'ws://192.168.1.11:8000/ws/notifications?token=$jwt';
+String wsNotificationsUrl(String jwt) {
+  if (apiBaseUrl.contains('onrender.com')) {
+    return 'wss://pmg-es-2025-2-ti5-6904100-kontatech.onrender.com/ws/notifications?token=$jwt';
+  }
+  return 'ws://192.168.1.11:8000/ws/notifications?token=$jwt';
+}
