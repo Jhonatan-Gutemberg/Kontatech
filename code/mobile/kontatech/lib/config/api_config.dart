ea5b57c2583a71f43c0abf3cc3425c1ef67783d6
Jhonatan-Gutemberg <mecjgr@hotmail.com>
2026-06-29 00:29:34 -0300

    Alteração de endpoint

--- code/mobile/kontatech/lib/config/api_config.dart
+++ code/mobile/kontatech/lib/config/api_config.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/foundation.dart';
 
 String get apiBaseUrl {
-  return 'https://kontatech-backend.onrender.com';
+  return 'https://kontatech-backend.onrender.com/api';
 
   /*
   if (kIsWeb) {
-    return 'http://localhost:8000';
+    return 'http://localhost:8000/api';
   }
   switch (defaultTargetPlatform) {
     case TargetPlatform.android:
-      return 'http://10.0.2.2:8000';
+      return 'http://10.0.2.2:8000/api';
     case TargetPlatform.iOS:
-      return 'http://127.0.0.1:8000';
+      return 'http://127.0.0.1:8000/api';
     default:
-      return 'http://192.168.1.11:8000';
+      return 'http://192.168.1.11:8000/api';
   }
   */
 }
